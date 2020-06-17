69d7542f35e6e956a9ea3a18f6c998981251fbf2
Atom <424534558@qq.com>
2020-06-17 17:45:51 +0800

    Initial commit

---
+++ src/main/java/com/atomjob/BayesSpamApplication.java
@@ -0,0 +1,13 @@
+package com.atomjob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BayesSpamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BayesSpamApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/atomjob/BayesSpamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atomjob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BayesSpamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
